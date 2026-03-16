4f8836c0ed1315366a5c68922effbf165f1ccf77
Zetsubo887 <abdul.balogoun@etu.univ-tours.fr>
2026-03-16 12:17:24 +0100

    Ajout du fichier app.py pour la création de l'application Dash

---
+++ app.py
@@ -0,0 +1,30 @@
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+
+app = dash.Dash(
+    __name__, 
+    use_pages=True,      
+    external_stylesheets=[dbc.themes.FLATLY]
+)
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Affichage des données", href="/pages1")),
+        dbc.NavItem(dbc.NavLink("Comparaison entre regions", href="/pages2")),
+        dbc.NavItem(dbc.NavLink("Aide en ligne", href="/markdown")),
+    ],
+    brand="Avocado Analytics",
+    color="primary",
+    dark=True,
+    className="mb-4"
+)
+
+app.layout = html.Div([
+    navbar,
+    dash.page_container 
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
